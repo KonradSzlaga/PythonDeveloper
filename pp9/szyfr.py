32308f8b261f6abf48ae772a0937faecfd092c3f
Smyrol <konradszlaga23@live.com>
2024-11-09 16:26:36 +0100

    zajecia

---
+++ pp9/szyfr.py
@@ -0,0 +1,7 @@
+#szyfr
+#dla każdego znaku zmieniono czwarty bit na przeciwny - od najmniej znaczącego
+
+msg = "Xq|`gf(bm{|(nibfq)"
+
+#funkcja - pokazuje numer znaku w systemie z ztabeli ASCII
+print(ord("c"))
