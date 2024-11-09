555a04558aa9595349bacd1228d665f78e68e114
Smyrol <konradszlaga23@live.com>
2024-11-09 14:23:17 +0100

    zajecia

--- pp8/ziarna.py
+++ pp8/ziarna.py
@@ -1,8 +1,14 @@
 i = 1
-sum = 1
+s = 1
 
-while i <= 8:
-    sum *= 2
+while i <= 64:
+    s *= 2
     i += 1
 else:
-    print("Wynik to", sum - 1, "ziaren.")
+    print("Wynik to", s - 1, "ziaren.")
+
+#Waga jedngo ziarna to 0,4mg ~ 0,0004g
+kg = 0.0004
+
+
+print(s * kg)
