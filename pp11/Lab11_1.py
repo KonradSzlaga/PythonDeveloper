fdbf0f6fa3f4c2a24755f9609fafbadff9c26310
Smyrol <konradszlaga23@live.com>
2024-12-07 09:47:09 +0100

    zajecia

---
+++ pp11/Lab11_1.py
@@ -0,0 +1,53 @@
+# Gra losowa
+import random as rd
+
+hits = 0
+generated_numbers = []
+
+
+print("Podaj 6 liczb całkowitych z zakresu od 1 do 49")
+
+number_list = []
+
+for i in range(6):
+    print("Podaj", i + 1, "liczbę: ", end="")
+    number = int(input())
+    number_list.append(number)
+
+number_list.sort()
+
+print(number_list)
+
+
+
+
+
+
+generated_numbers = rd.sample(range(1, 50), 6) #bez powtórzeń
+
+generated_numbers.sort()
+
+print(generated_numbers)
+
+
+
+
+
+
+#Mechanizm sprawdzający
+
+for number in number_list:
+    if number in generated_numbers:
+        hits += 1
+
+print("Trafiono", hits, "razy." )
+
+
+
+
+
+
+
+
+
+
---
+++ pp11/Lab11_2.py
@@ -0,0 +1,26 @@
+
+#1.Pobranie liczb od użytkownika
+#2.Usuwanie duplikatów
+#3.Sortowanie i wyświetlenie.
+import random as rd
+
+
+numbers = []
+
+numbers_total = int(input("Ile liczb chcesz podać: "))
+
+for i in range(numbers_total):
+    number = int(input("Podaj " + str(i+1) + " element zbioru: " ))
+    numbers.append(number)
+
+
+numbers_distinct = []
+
+for number in numbers:
+    if number not in numbers_distinct:
+        numbers_distinct.append(number)
+
+
+numbers_distinct.sort(reverse=True)
+
+print("Posortowany malejąco zbiór to:", numbers_distinct)
