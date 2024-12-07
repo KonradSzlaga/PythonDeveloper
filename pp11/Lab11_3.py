ad53f5502176fed81ffb3a7bc591f681b0a0e6a1
Smyrol <konradszlaga23@live.com>
2024-12-07 10:04:32 +0100

    zajecia

---
+++ pp11/Lab11_3.py
@@ -0,0 +1,24 @@
+# Wirtualna szachownica
+# Rozmieszczenie pionów
+# Wyświetlenie
+
+import random as rd
+
+blank_square = "--"
+Pieces = ["wp", "bp", "bt", "bt", "wq"]
+
+chess_board = [[blank_square for _ in range(8)] for _ in range(8)]
+
+counter = 0
+
+while counter < len(Pieces):
+    row = rd.randint(0, 7)
+    col = rd.randint(0, 7)
+    if chess_board[row][col] == blank_square:
+        chess_board[row][col] = Pieces[counter]
+        counter += 1
+
+for row in range(len(chess_board)):
+    for col in range(len(chess_board[row])):
+        print(chess_board[row][col], end=" ")
+    print()
