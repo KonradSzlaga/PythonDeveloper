995c31297aa8f00efc3ea48502e58e319ee37175
Konrad Szlaga <konrad.szlaga@pl.abb.com>
2025-01-30 16:15:14 +0100

    nowe zadania

---
+++ pp17/app0.py
@@ -0,0 +1,11 @@
+import package1.module1  as i1
+import package1.module2 as i2
+
+
+
+
+i1.introduce()
+i2.introduce()
+
+
+print(dir())
---
+++ pp17/app3.py
@@ -0,0 +1,10 @@
+import sys
+
+
+
+sys.path.append(r"C:\Users\konrad.szlaga\Desktop")
+#dziaął też z plikami .zip
+
+from package2 import module1
+
+module1.introduce()
